1d3d2c996c510fe20f2f34e65c26b37efd3cff84
ddhoogduin <ddhoogduin@gmail.com>
2020-04-29 17:43:32 +0200

    modify main plot

--- src/hooks/linecharts-tabs.jsx
+++ src/hooks/linecharts-tabs.jsx
@@ -3,11 +3,8 @@ import {Icon, Menu, Tab} from "semantic-ui-react";
 import createPlotlyComponent from 'react-plotly.js/factory';
 import Plotly from 'plotly.js-basic-dist'
 import SizeMe from 'react-sizeme'
+import medication from "../utils/fakeData/medication";
 const Plot = createPlotlyComponent(Plotly);
-
-const hb_options = [72, 73, 73, 74, 74, 74, 75, 75, 75, 75, 75,75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 76, 76, 76, 76, 77, 77,78]
-const bp_high_options = [ 84,84,84,84,84,84,84,84,84,84,83, 85, 85, 85, 85, 85, 85]
-const bp_low_options = [55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55, 60,60,60, 60,60,60, 60,60,60, 60,60, 60,60, 60,60]
 
 
 
@@ -20,15 +17,24 @@ const fakeData = (n) =>{
 }
 
 class LinechartsTabs extends React.Component{
-    state = {data1:{}, data2:{}}
+    state = {data1:{}, data2:{}, data3:{}}
 
     panes = [
         {
             menuItem: 'Mocusal Cytokines',
             render: () => <Tab.Pane  attached={true} as={'div'}>
                 <Plot
-                    className={'full-size'}
+                    className={'full-size large'}
                     data={this.state.data1}
+                    layout={{
+                        yaxis:{
+                            range: [0, 10000]
+                        },
+                        xaxis:{
+                            range: ['2020-04-01', '2020-04-29'],
+                            type: 'date'
+                        }
+                    }}
                 />
             </Tab.Pane>,
         },
@@ -45,16 +51,17 @@ class LinechartsTabs extends React.Component{
             menuItem: 'Viral loads',
             render: () => <Tab.Pane attached={false} as={'div'}>Tab 3 Content</Tab.Pane>,
         },
+
     ]
     componentDidMount() {
         this.getPlotTracks()
     }
-
-    updateData = (item, options) =>{
-        item.y.shift()
-        item.y.push(options[Math.floor(Math.random() * options.length)])
-        item.string =Math.random().toString(36).substring(7)
-        return item
+    generateFakeDates = () =>{
+        const items = []
+        for(let i =1; i < 30; i++){
+            items.push(`2020-04-${i}`)
+        }
+        return items
     }
     getPlotTracks = () =>{
         const names = ['IL6-IL1-TNF', 'CD30/TNFRSF8', 'IL-6', 'IL-1b', 'TNF-a']
@@ -66,7 +73,7 @@ class LinechartsTabs extends React.Component{
                 visible = true
             return {
                 name:name,
-                x: Array.from(Array(200).keys()),
+                x: this.generateFakeDates(),
                 y:fakeData(30),
                 type: 'scatter',
                 mode: 'lines',
@@ -75,6 +82,22 @@ class LinechartsTabs extends React.Component{
                 string:Math.random().toString(36).substring(7)
             }
         })
+
+        const medication_data = medication.map((item)=>{
+            return {
+                name:item.name,
+                x: item.periods,
+                y: item.y,
+                type: 'line',
+                mode: 'lines',
+                hoverinfo:"name",
+                hoverlabel: {namelength :-1},
+                line:{
+                    width: 10
+                }
+            }
+        })
+        const data1 = [...data, ...medication_data]
         const data2 = names2.map((name) =>{
             let visible = "legendonly"
             if(['IL7'].includes(name))
@@ -90,7 +113,22 @@ class LinechartsTabs extends React.Component{
                 string:Math.random().toString(36).substring(7)
             }
         })
-        this.setState({data1: data, data2: data2})
+        const data3 = names2.map((name) =>{
+            return {
+                name:name,
+                x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00'],
+                y:[3,3],
+                type: 'line',
+                mode: 'lines+text',
+                text: "test",
+                textposition:"center",
+                line: {
+                    color: 'rgb(55, 128, 191)',
+                    width: 20
+                }
+            }
+        })
+        this.setState({data1: data1, data2: data2, data3:data3})
     }
 
     render() {
